f234ad139ae01c54f4d59bbba5732fb00b9584df
kondounagi <kondounagi@gmail.com>
2020-10-29 15:27:16 +0900

    Change input image shape

--- 3rd/platform/model_development/quantization.sh
+++ 3rd/platform/model_development/quantization.sh
@@ -1,9 +1,9 @@
 vai_q_tensorflow quantize \
-  --input_frozen_graph frozen_resnet_v1_50.pb \
+  --input_frozen_graph frozen_bisenet_v1_0.pb \
   --input_nodes input \
-  --input_shapes ?,224,224,3 \
-  --output_nodes resnet_v1_50/predictions/Reshape_1 \
-  --input_fn resnet_v1_50_input_fn.calib_input \
+  --input_shapes ?,448,704,3 \
+  --output_nodes resnet_v1_0/predictions/Reshape_1 \
+  --input_fn bisenet_v1_0_input_fn.calib_input \
   --method 1 \
   --gpu 0 \
   --output_dir ./quantize_results \
