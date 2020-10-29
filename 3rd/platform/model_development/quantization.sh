a68a05dc193c80c0dffa62fbd9f330246a99d9ad
kondounagi <kondounagi@gmail.com>
2020-10-29 19:19:47 +0900

    Change input_shape and output_node name

--- 3rd/platform/model_development/quantization.sh
+++ 3rd/platform/model_development/quantization.sh
@@ -1,8 +1,8 @@
 vai_q_tensorflow quantize \
   --input_frozen_graph frozen_bisenet_v1_0.pb \
   --input_nodes input \
-  --input_shapes ?,448,704,3 \
-  --output_nodes resnet_v1_0/predictions/Reshape_1 \
+  --input_shapes ?,1216,1936,3 \
+  --output_nodes bisenet_v1_0/predictions/Reshape_1 \
   --input_fn bisenet_v1_0_input_fn.calib_input \
   --method 1 \
   --gpu 0 \
