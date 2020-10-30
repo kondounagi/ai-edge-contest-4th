2a77c1ad47526372491426869ee7c314aa1b0f66
Yuuki Sawabe <kabuto6419@g.ecc.u-tokyo.ac.jp>
2020-10-30 12:19:32 +0900

    feat: reverse data from pretrain_val to pretrain_train

--- models/BiSeNet/datasets/make_pretrain_val.py
+++ models/BiSeNet/datasets/make_pretrain_val.py
@@ -27,32 +27,60 @@ def main():
     # destination
     img_folder_val_pre = 'pretrain/val/img'
     lb_folder_val_pre = 'pretrain/val/lb'
+    
+    # randomに選んだimgとlbをvalidationセットとして，trainからvalに移す
+    if not args.reverse:
+        img_paths_pre = os.listdir(img_folder_pre)
+        lb_paths_pre = os.listdir(lb_folder_pre)
+        pretrain_size = len(img_paths_pre) # 移動する前のpretrain/train内のデータ数
 
-    # randomに選んだimgとlbをvalidationセットとして，valに移す
-    img_paths_pre = os.listdir(img_folder_pre)
-    lb_paths_pre = os.listdir(lb_folder_pre)
-    pretrain_size = len(img_paths_pre) # 移動する前のpretrain内のデータ数
-    
-    img_paths_pre.sort()
-    lb_paths_pre.sort()
-    '''
-    for i in range(pretrain_size):
-        print(img_paths_pre[i], lb_paths_pre[i])
-    print(pretrain_size)
-    '''
-    assert len(img_paths_pre) == len(lb_paths_pre)
-    
-    val_size = args.val_size # valに移す枚数
-    assert pretrain_size > val_size
+        img_paths_pre.sort()
+        lb_paths_pre.sort()
+        '''
+        for i in range(pretrain_size):
+            print(img_paths_pre[i], lb_paths_pre[i])
+        '''
+        assert len(img_paths_pre) == len(lb_paths_pre)
+        print("current train size: ", pretrain_size)
+        
+        val_size = args.val_size # valに移す枚数
+        assert pretrain_size > val_size
 
-    # valとするpretrainデータののindexをランダムに生成
-    val_indices = random.sample(range(0, pretrain_size, 1), k=val_size) 
-    print("selected indices for validation: ")
-    print(val_indices)
+        # valとするpretrainデータののindexをランダムに生成
+        val_indices = random.sample(range(0, pretrain_size, 1), k=val_size) 
+        print("val_size: ", val_size)
+        print("selected indices for validation: ")
+        print(val_indices)
+
+        for index in tqdm(val_indices):
+            shutil.move(os.path.join(img_folder_pre, img_paths_pre[index]), img_folder_val_pre)
+            shutil.move(os.path.join(lb_folder_pre, lb_paths_pre[index]), lb_folder_val_pre)
+        
+        print("done")
     
-    for index in tqdm(val_indices):
-        shutil.move(os.path.join(img_folder_pre, img_paths_pre[index]), img_folder_val_pre)
-        shutil.move(os.path.join(lb_folder_pre, lb_paths_pre[index]), lb_folder_val_pre)
+    # reverse: valからtrainにデータを戻す
+    else:
+        img_paths_val_pre = os.listdir(img_folder_val_pre)
+        lb_paths_val_pre = os.listdir(lb_folder_val_pre)
+        pretrain_val_size = len(img_paths_val_pre) # 移動する前のpretrain/val内のデータ数
+        
+        img_paths_val_pre.sort()
+        lb_paths_val_pre.sort()
+        
+        '''
+        for i in range(pretrain_val_size):
+            print(img_paths_val_pre[i], lb_paths_val_pre[i])
+        '''
+        assert len(img_paths_val_pre) == len(lb_paths_val_pre)
+        print("current val size: ", pretrain_val_size)
+        
+        
+        for index in tqdm(range(0, pretrain_val_size, 1)):
+            shutil.move(os.path.join(img_folder_val_pre, img_paths_val_pre[index]), img_folder_pre)
+            shutil.move(os.path.join(lb_folder_val_pre, lb_paths_val_pre[index]), lb_folder_pre)
+        
+        print("done")
+
 
 if __name__ == '__main__':
     main()
